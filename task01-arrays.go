e7158ce4e22d9ff9978692470d40b89f6dff0889
rcmgn <rcmgn@yandex.ru>
2022-07-09 20:08:46 +0500

    Task 1-2

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,9 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	for i := 0; i < len(input); i++ {
-		result += input[i]
+	for _, i := range input {
+		result += i
 	}
 	result = result / float32(len(input))
-	return
+	return result
 }
