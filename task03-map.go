627a0e3dee47fcb2639217d46aa6597483bc8f90
rcmgn <rcmgn@yandex.ru>
2022-07-10 17:58:39 +0500

    Solve Task3

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,15 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	keys := make([]int, 0, len(input))
+	for key := range input {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
+		result = append(result, input[key])
+	}
 	return
 }
